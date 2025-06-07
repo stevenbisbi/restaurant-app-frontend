d5ab33c0be3987e9acf553937cc28ba880f069f9
Michael <stevenex13@gmail.com>
2025-06-07 13:20:14 -0500

    cambio baseurl

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
